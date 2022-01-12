05f94fc3ae50ed23449834b1278f6c09a7bb60c8
foreverdevstar <foreverdevstar@gmail.com>
2022-01-12 22:59:52 +0530

    init

---
+++ Frontend/Components/Navbar.php
@@ -0,0 +1,21 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+        <a class="navbar-brand" href="index.php">
+            <img src="./Images/favicon.png" alt="" width="30" height="30" class="d-inline-block align-text-top">
+            Markswise
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav ms-auto">
+                <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                <a class="nav-link active" aria-current="page" href="usage.php">Usage</a>
+                <a class="nav-link active" aria-current="page" href="help.php"><i class="fas fa-question-circle"></i> Help</a>
+                <button class="btn btn-primary login-btn">Login</button>
+                <button class="btn btn-primary signup-btn">Create Account</button>
+            </div>
+        </div>
+    </div>
+</nav>
