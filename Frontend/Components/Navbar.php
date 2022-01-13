f127032bf350420208b04f9ad7c07bcd82a76169
foreverdevstar <foreverdevstar@gmail.com>
2022-01-13 09:41:40 +0530

    signup modal created

--- Frontend/Components/Navbar.php
+++ Frontend/Components/Navbar.php
@@ -1,5 +1,5 @@
 <?php include 'loginModal.php' ?>
-
+<?php include 'signupModal.php' ?>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container">
         <a class="navbar-brand" href="index.php">
---
+++ Frontend/Components/Signupmodal.php
@@ -0,0 +1,37 @@
+<!-- Modal -->
+<form method="POST" action="">
+    <div class="modal fade" id="signupModal" tabindex="-1" aria-labelledby="signupModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="signupModalLabel">Signup</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <!-- form inside modal -->
+                    <div class="mb-3">
+                        <label for="schoolname" class="form-label">School Name</label>
+                        <input type="text" class="form-control" id="schoolname">
+                    </div>
+                    <div class="mb-3">
+                        <label for="email" class="form-label">Email address</label>
+                        <input type="email" class="form-control" id="email" aria-describedby="emailHelp">
+                        <div id="emailHelp" class="form-text">We'll not spam to your e-mail.</div>
+                    </div>
+                    <div class="mb-3">
+                        <label for="password" class="form-label">Password</label>
+                        <input type="password" class="form-control" id="password">
+                    </div>
+                    <div class="mb-3">
+                        <label for="password" class="form-label"> Confirm Password</label>
+                        <input type="password" class="form-control" id="password">
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary">Create Account</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</form>
