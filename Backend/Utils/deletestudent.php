9d4975f41b9bc6876ea368587bdecf74f91b52e4
foreverdevstar <foreverdevstar@gmail.com>
2022-01-14 19:07:00 +0530

    delete operation bug fixed!

--- Backend/Utils/deletestudent.php
+++ Backend/Utils/deletestudent.php
@@ -1,8 +1,17 @@
 <?php
-if(isset($_GET['deleteid']) && isset($_GET['schoolid']) && isset($_GET['sectionid']))
+if(isset($_GET['deleteid']))
 {   
+        $classid = $_GET['classid'];
+        $sectionid = $_GET['sectionid'];
+        $schoolid = $_GET['schoolid'];
+
         $del_id = $_GET['deleteid'];
         $deletequery = "DELETE FROM studentsmarks WHERE student_unique_id = '$del_id'";
         $run_delete = mysqli_query($connection, $deletequery);
+        
+        echo
+        '<script>
+        window.location.href = "class.php?classid='.$classid.'&sectionid='.$sectionid.'&schoolid='.$schoolid.'";
+        </script>';
 }
 ?>
